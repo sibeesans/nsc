5551a9d8fa2591dafbcb1cd3893d76ae9a467c21
Rega23 <92116230+Rega23@users.noreply.github.com>
2022-12-01 20:35:29 +0700

    Update updatee.sh

--- menu/updatee.sh
+++ menu/updatee.sh
@@ -10,12 +10,14 @@ wget -O del-xr "https://raw.githubusercontent.com/Rega23/new-sc/main/menu/del-xr
 wget -O del-xvless "https://raw.githubusercontent.com/Rega23/new-sc/main/menu/del-xvless.sh"
 wget -O xp-xr "https://raw.githubusercontent.com/Rega23/new-sc/main/menu/xp-xr.sh"
 wget -O xp-xvless "https://raw.githubusercontent.com/Rega23/new-sc/main/menu/xp-xvless.sh"
+wget -O menu "https://raw.githubusercontent.com/Rega23/new-sc/main/menu/menu.sh"
 chmod +x add-xr
 chmod +x add-xvless
 chmod +x del-xr
 chmod +x del-xvless
 chmod +x xp-xr
 chmod +x xp-xvless
+chmod +x menu
 #enc
 shc -r -f add-xr -o add-xr
 shc -r -f add-xvless -o add-xvless
@@ -23,6 +25,7 @@ shc -r -f del-xr -o del-xr
 shc -r -f del-xvless -o del-xvless
 shc -r -f xp-xr -o xp-xr
 shc -r -f xp-xvless -o xp-xvless
+shc -r -f xp-xvless -o menu
 echo "0 0 * * * root xp-xr" >> /etc/crontab
 echo "0 0 * * * root xp-xvless" >> /etc/crontab
 clear
