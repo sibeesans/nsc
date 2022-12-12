d8cb0bbc37b99aa10e756c2a64e6b33b2494aff1
sibeesans <105978585+sibeesans@users.noreply.github.com>
2022-12-12 20:14:09 +0700

    Update info.sh

--- menu/info.sh
+++ menu/info.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
+dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
+#########################
+
 MYIP=$(wget -qO- ipinfo.io/ip);
+echo "Checking VPS"
+IZIN=$( curl ipinfo.io/ip | grep $MYIP )
+if [ $MYIP = $MYIP ]; then
+echo -e "${NC}${GREEN}Permission Accepted...${NC}"
+else
+echo -e "${NC}${RED}Permission Denied!${NC}";
+echo -e "${NC}${LIGHT}Fuck You!!"
+exit 0
+fi
 
 clear
 sttt=`cat /root/log-install.txt`
