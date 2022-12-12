29877d5ddc01575b909253a9692de3cedfd3c2cb
sibeesans <105978585+sibeesans@users.noreply.github.com>
2022-12-12 22:09:58 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -49,9 +49,9 @@ echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━�
 apt install jq curl -y
 DOMAIN=arshaka.tech
 sub=$(</dev/urandom tr -dc a-z | head -c4)
-SUB_DOMAIN=${sub}.arshaka.tech
-CF_ID=rega.andriana@gmail.com
-CF_KEY=7fa393c334da66a56b439deb29db45ca546a0
+SUB_DOMAIN=${sub}.sibe-vpn.live
+CF_ID=anfuji184@gmail.com
+CF_KEY=8700ccfd51dec7e1c3c95dd44250c7891b913
 set -euo pipefail
 IP2=$(wget -qO- ipinfo.io/ip);
 echo "Updating DNS for ${SUB_DOMAIN}..."
