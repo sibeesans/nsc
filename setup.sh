0a138a76067aa6e67375cfeed0b2229858be0d5f
sibeesans <105978585+sibeesans@users.noreply.github.com>
2022-12-12 22:47:34 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -47,8 +47,8 @@ echo -e "$green Silahkan masukan sub domain anda $NC"
 echo -e "$green Jika tidak punya silahkan klik [ Ctrl+C ] • To-Exit $NC"
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 apt install jq curl -y
-DOMAIN=arshaka.tech
-sub=$(</dev/urandom tr tr -dc a-z0-9 | head -c4)
+DOMAIN=sibe-vpn.live
+sub=$(</dev/urandom tr -dc a-z0-9 | head -c5)
 SUB_DOMAIN=${sub}.sibe-vpn.live
 CF_ID=anfuji184@gmail.com
 CF_KEY=8700ccfd51dec7e1c3c95dd44250c7891b913
