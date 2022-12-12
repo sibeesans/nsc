f6209b4ec6ef921cacaac65d3a9f690afc920032
sibeesans <105978585+sibeesans@users.noreply.github.com>
2022-12-12 21:48:34 +0700

    Update xp.sh

--- menu/xp.sh
+++ menu/xp.sh
@@ -12,19 +12,27 @@ sed -i '/^,"'"$user"'"$/d' /etc/trojan/config.json
 fi
 done
 systemctl restart trojan
-data=( `cat /etc/wireguard/wg0.conf | grep '^### Client' | cut -d ' ' -f 3`);
-now=`date +"%Y-%m-%d"`
-for user in "${data[@]}"
-do
-exp=$(grep -w "^### Client $user" "/etc/wireguard/wg0.conf" | cut -d ' ' -f 4)
+fi
+done
+exp=$(grep -w "^### $user" "/etc/v2ray/config.json" | cut -d ' ' -f 3)
 d1=$(date -d "$exp" +%s)
 d2=$(date -d "$now" +%s)
 exp2=$(( (d1 - d2) / 86400 ))
 if [[ "$exp2" = "0" ]]; then
-sed -i "/^### Client $user $exp/,/^AllowedIPs/d" /etc/wireguard/wg0.conf
-rm -f "/home/vps/public_html/$user.conf"
+sed -i "/^### $user $exp/,/^},{/d" /etc/v2ray/config.json
+sed -i "/^### $user $exp/,/^},{/d" /etc/v2ray/none.json
+rm -f /etc/v2ray/$user-tls.json /etc/v2ray/$user-none.json
 fi
 done
+systemctl restart v2ray
+systemctl restart v2ray@none
+data=( `cat /etc/v2ray/vless.json | grep '^###' | cut -d ' ' -f 2`);
+now=`date +"%Y-%m-%d"`
+for user in "${data[@]}"
+data=( `cat /etc/trojan/akun.conf | grep '^###' | cut -d ' ' -f 2`);
+now=`date +"%Y-%m-%d"`
+for user in "${data[@]}"
+do
 systemctl restart v2ray
 systemctl restart v2ray@none
 data=( `cat /etc/v2ray/vless.json | grep '^###' | cut -d ' ' -f 2`);
